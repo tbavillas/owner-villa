1ae7c837cbc968df7a97e94fadaf6dd8f79c5a1d
wilantarajaya <wilantarajaya.123@gmail.com>
2024-10-02 13:23:36 +0800

    update backound

--- src/app/villas/[slug]/page.js
+++ src/app/villas/[slug]/page.js
@@ -642,7 +642,7 @@ export default function Villas({ params }) {
               </div>
             </div>
           </div>
-          {/* <div className="opacity-75 fixed inset-0 z-40 bg-black"></div> */}
+          <div className="opacity-75 fixed inset-0 z-40 bg-black"></div>
         </>
       )}
     </main>
